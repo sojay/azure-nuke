2fda68dc545edad419cedfe781862a2e1178ed6f
Sammie <thesamokorie@gmail.com>
2025-07-07 03:09:34 +0000

    Fix homebrew formula - use pip install method for proper dependency handling

--- homebrew/aznuke.rb
+++ homebrew/aznuke.rb
@@ -1,14 +1,21 @@
 class Aznuke < Formula
   desc "A powerful CLI tool for scanning and cleaning up Azure resources"
   homepage "https://github.com/sojay/azure-nuke"
-  url "https://github.com/sojay/azure-nuke/archive/v0.1.3.tar.gz"
+  url "https://github.com/sojay/azure-nuke/archive/v0.1.5.tar.gz"
   sha256 "PLACEHOLDER_SHA256"
   license "MIT"
 
   depends_on "python@3.11"
 
   def install
-    virtualenv_install_with_resources
+    venv = virtualenv_create(libexec, "python3.11")
+    venv.pip_install buildpath
+    
+    # Create wrapper script
+    (bin/"aznuke").write <<~EOS
+      #!/usr/bin/env bash
+      exec "#{libexec}/bin/python" -m aznuke "$@"
+    EOS
   end
 
   test do
