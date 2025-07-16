f7fedd3e1b052aebc563abb0af98eaa91d4b6338
Sammie <thesamokorie@gmail.com>
2025-07-16 23:24:51 +0000

    Fix Homebrew setup: update SHA256 and disable conflicting workflow

--- homebrew/aznuke.rb
+++ homebrew/aznuke.rb
@@ -2,7 +2,7 @@ class Aznuke < Formula
   desc "A powerful CLI tool for scanning and cleaning up Azure resources"
   homepage "https://github.com/sojay/azure-nuke"
   url "https://github.com/sojay/azure-nuke/archive/v0.1.7.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "96171ad6a0218f74f8829a3bcdb36dfdd2a4fce45e1f0b0203a774876ec71d41"
   license "MIT"
 
   depends_on "python@3.11"
